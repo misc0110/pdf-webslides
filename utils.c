39c7a5c725e631b2e899f7594a5b176cc07b1d8d
Michael Schwarz <michael.schwarz91@gmail.com>
2020-03-22 16:35:28 +0100

    format

--- utils.c
+++ utils.c
@@ -1,149 +1,149 @@
-#include "utils.h" 
-#include "webslides.h"
+#include "utils.h"
+
 #include <stdlib.h>
 #include <string.h>
-        
-int base64encode(const void* data_buf, size_t dataLength, char* result, size_t resultSize)
-{
-   const char base64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-   const uint8_t *data = (const uint8_t *)data_buf;
-   size_t resultIndex = 0;
-   size_t x;
-   uint32_t n = 0;
-   int padCount = dataLength % 3;
-   uint8_t n0, n1, n2, n3;
 
-   for (x = 0; x < dataLength; x += 3) 
-   {
-      n = ((uint32_t)data[x]) << 16;
-      
-      if((x+1) < dataLength)
-         n += ((uint32_t)data[x+1]) << 8;
-      
-      if((x+2) < dataLength)
-         n += data[x+2];
+#include "webslides.h"
 
-      n0 = (uint8_t)(n >> 18) & 63;
-      n1 = (uint8_t)(n >> 12) & 63;
-      n2 = (uint8_t)(n >> 6) & 63;
-      n3 = (uint8_t)n & 63;
-            
-      if(resultIndex >= resultSize) return 1;
-      result[resultIndex++] = base64chars[n0];
-      if(resultIndex >= resultSize) return 1;
-      result[resultIndex++] = base64chars[n1];
+// ---------------------------------------------------------------------------
+int base64encode(const void *data_buf, size_t dataLength, char *result,
+                 size_t resultSize) {
+  const char base64chars[] =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  const uint8_t *data = (const uint8_t *)data_buf;
+  size_t resultIndex = 0;
+  size_t x;
+  uint32_t n = 0;
+  int padCount = dataLength % 3;
+  uint8_t n0, n1, n2, n3;
 
-      if((x+1) < dataLength)
-      {
-         if(resultIndex >= resultSize) return 1;
-         result[resultIndex++] = base64chars[n2];
+  for (x = 0; x < dataLength; x += 3) {
+    n = ((uint32_t)data[x]) << 16;
+
+    if ((x + 1) < dataLength) n += ((uint32_t)data[x + 1]) << 8;
+    if ((x + 2) < dataLength) n += data[x + 2];
+
+    n0 = (uint8_t)(n >> 18) & 63;
+    n1 = (uint8_t)(n >> 12) & 63;
+    n2 = (uint8_t)(n >> 6) & 63;
+    n3 = (uint8_t)n & 63;
+
+    if (resultIndex >= resultSize) return 1;
+    result[resultIndex++] = base64chars[n0];
+    if (resultIndex >= resultSize) return 1;
+    result[resultIndex++] = base64chars[n1];
+
+    if ((x + 1) < dataLength) {
+      if (resultIndex >= resultSize) return 1;
+      result[resultIndex++] = base64chars[n2];
+    }
+    if ((x + 2) < dataLength) {
+      if (resultIndex >= resultSize) return 1;
+      result[resultIndex++] = base64chars[n3];
+    }
+  }
+  if (padCount > 0) {
+    for (; padCount < 3; padCount++) {
+      if (resultIndex >= resultSize) return 1;
+      result[resultIndex++] = '=';
+    }
+  }
+  if (resultIndex >= resultSize) return 1;
+  result[resultIndex] = 0;
+  return 0;
+}
+
+// ---------------------------------------------------------------------------
+char *read_file(char *fname) {
+  FILE *f = fopen(fname, "rb");
+  if (!f) {
+    return NULL;
+  }
+  fseek(f, 0, SEEK_END);
+  size_t s = ftell(f);
+  fseek(f, 0, SEEK_SET);
+  char *in = calloc(s + 1, 1);
+  fread(in, s, 1, f);
+  fclose(f);
+  return in;
+}
+
+// ---------------------------------------------------------------------------
+char *encode_file_base64(char *fname) {
+  FILE *f = fopen(fname, "rb");
+  fseek(f, 0, SEEK_END);
+  size_t s = ftell(f);
+  fseek(f, 0, SEEK_SET);
+  char *in = malloc(s);
+  fread(in, s, 1, f);
+  fclose(f);
+  char *out = malloc(s * 2 + 8);
+  base64encode(in, s, out, s * 2 + 8);
+  free(in);
+  return out;
+}
+
+// ---------------------------------------------------------------------------
+char *encode_array_base64(char *array, size_t len) {
+  char *out = malloc(len * 2 + 8);
+  base64encode(array, len, out, len * 2 + 8);
+  return out;
+}
+
+// ---------------------------------------------------------------------------
+char *replace_string_first(char *input, char *search, char *replace) {
+  size_t search_len = strlen(search);
+  size_t input_len = strlen(input);
+  size_t replace_len = strlen(replace);
+  char *nbuff = malloc(input_len - search_len + replace_len + 1);
+  size_t i, out = 0;
+  for (i = 0; i < input_len; i++) {
+    if (i < input_len - search_len &&
+        strncmp(input + i, search, search_len) == 0) {
+      for (size_t j = 0; j < replace_len; j++) {
+        nbuff[out++] = replace[j];
       }
-      if((x+2) < dataLength)
-      {
-         if(resultIndex >= resultSize) return 1;
-         result[resultIndex++] = base64chars[n3];
-      }
-   }
-   if (padCount > 0) 
-   { 
-      for (; padCount < 3; padCount++) 
-      { 
-         if(resultIndex >= resultSize) return 1;
-         result[resultIndex++] = '=';
-      } 
-   }
-   if(resultIndex >= resultSize) return 1;
-   result[resultIndex] = 0;
-   return 0;
-}
-
-
-char* read_file(char* fname) {
-    FILE* f = fopen(fname, "rb");
-    if(!f) {
-      return NULL;
+      i += search_len - 1;
+    } else {
+      nbuff[out++] = input[i];
     }
-    fseek(f, 0, SEEK_END);
-    size_t s = ftell(f);
-    fseek(f, 0, SEEK_SET);
-    char* in = calloc(s + 1, 1);
-    fread(in, s, 1, f);
-    fclose(f);
-    return in;   
+  }
+  nbuff[out] = 0;
+  free(input);
+  return nbuff;
 }
 
-char* encode_file_base64(char* fname) {
-    FILE* f = fopen(fname, "rb");
-    fseek(f, 0, SEEK_END);
-    size_t s = ftell(f);
-    fseek(f, 0, SEEK_SET);
-    char* in = malloc(s);
-    fread(in, s, 1, f);
-    fclose(f);
-    char* out = malloc(s * 2 + 8);
-    base64encode(in, s, out, s * 2 + 8);
-    free(in);
-    return out;
-}
-
-
-char* encode_array_base64(char* array, size_t len) {
-    char* out = malloc(len * 2 +  8);
-    base64encode(array, len, out, len * 2 + 8);
-    return out;
-}
-
-
-char* replace_string_first(char* input, char* search, char* replace) {
-    size_t search_len = strlen(search);
-    size_t input_len = strlen(input);
-    size_t replace_len = strlen(replace);
-    char* nbuff = malloc(input_len - search_len + replace_len + 1);
-    size_t i, out = 0;
-    for(i = 0; i < input_len; i++) {
-        if(i < input_len - search_len && strncmp(input + i, search, search_len) == 0) {
-            for(size_t j = 0; j < replace_len; j++) {
-                nbuff[out++] = replace[j];
-            }
-            i += search_len - 1;
-        } else {
-            nbuff[out++] = input[i];
-        }
+// ---------------------------------------------------------------------------
+char *encode_array(SlideInfo *info, int offset, int len, int b64,
+                   progress_t cb) {
+  // calculate size
+  size_t rlen = 0;
+  for (int i = 0; i < len; i++) {
+    char *d = ((char **)(info + i))[offset];
+    if (d) {
+      rlen += strlen(d) * (b64 + 1) + 8;
+    } else {
+      rlen += 8;
     }
-    nbuff[out] = 0;
-    free(input);
-    return nbuff;
-}
+  }
+  char *buff = malloc(rlen);
+  strcpy(buff, "[");
 
-char* encode_array(SlideInfo* info, int offset, int len, int b64, progress_t cb) {
-    // calculate size
-    size_t rlen = 0;
-    for(int i = 0; i < len; i++) {
-        char* d = ((char**)(info + i))[offset];
-        if(d) {
-            rlen += strlen(d) * (b64 + 1) + 8;
-        } else {
-            rlen += 8;
-        }
+  for (int i = 0; i < len; i++) {
+    char *data = ((char **)(info + i))[offset];
+    char *b64_data = data;
+    if (b64) {
+      if (!data) data = "";
+      b64_data = calloc(strlen(data) * 2 + 8, 1);
+      base64encode(data, strlen(data), b64_data, strlen(data) * 2 + 8);
     }
-    char* buff = malloc(rlen);
-    strcpy(buff, "[");
+    strcat(buff, "\"");
+    strcat(buff, b64_data);
+    strcat(buff, "\"");
+    if (i != len - 1) strcat(buff, ", ");
+    if (cb) cb(i);
+  }
 
-    for(int i = 0; i < len; i++) {
-        char* data = ((char**)(info + i))[offset];
-        char* b64_data = data;
-        if(b64) {
-            if(!data) data = "";
-            b64_data = calloc(strlen(data) * 2 + 8, 1);
-            base64encode(data, strlen(data), b64_data, strlen(data) * 2 + 8);
-        }
-        strcat(buff, "\"");
-        strcat(buff, b64_data);
-        strcat(buff, "\"");
-        if(i != len - 1) strcat(buff, ", ");
-        if(cb) cb(i);
-    }
-    
-    strcat(buff, "]\n");
-    return buff;
+  strcat(buff, "]\n");
+  return buff;
 }
--- webslides.c
+++ webslides.c
@@ -1,73 +1,74 @@
+#include "webslides.h"
+
 #include <cairo-svg.h>
 #include <cairo.h>
 #include <poppler-document.h>
 #include <poppler-page.h>
 #include <poppler.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
-#include "colorprint_header.h"
+
 #include "cli.h"
+#include "colorprint_header.h"
 #include "res.h"
 #include "utils.h"
-#include "webslides.h"
 
 #define NO_SLIDES 0
 
-static getopt_arg_t cli_options[] =
-{
-        {"single",       no_argument,       NULL, 's', "Create a single file", NULL},
-        {"presenter",    no_argument,       NULL, 'p', "Start in presenter mode", NULL},
-        {"output",       required_argument, NULL, 'o', "Output file name", "FILENAME"}, 
-        {"disablenotes", no_argument,       NULL, 'n', "Do not include notes", NULL},
-        {"help",         no_argument,       NULL, 'h', "Show this help.",       NULL},
-        {NULL, 0,                        NULL, 0, NULL,                      NULL}
-};
+// ---------------------------------------------------------------------------
+static getopt_arg_t cli_options[] = {
+    {"single", no_argument, NULL, 's', "Create a single file", NULL},
+    {"presenter", no_argument, NULL, 'p', "Start in presenter mode", NULL},
+    {"output", required_argument, NULL, 'o', "Output file name", "FILENAME"},
+    {"disablenotes", no_argument, NULL, 'n', "Do not include notes", NULL},
+    {"help", no_argument, NULL, 'h', "Show this help.", NULL},
+    {NULL, 0, NULL, 0, NULL, NULL}};
 
-
-int convert(PopplerPage *page, const char *fname, SlideInfo* info) {
+// ---------------------------------------------------------------------------
+int convert(PopplerPage *page, const char *fname, SlideInfo *info) {
   cairo_surface_t *surface;
   cairo_t *img;
   double width, height;
-  char* comm = calloc(1024, 1);
+  char *comm = calloc(1024, 1);
 
   poppler_page_get_size(page, &width, &height);
   surface = cairo_svg_surface_create(fname, width, height);
   img = cairo_create(surface);
 
   poppler_page_render_for_printing(page, img);
-  
-  GList* annot_list = poppler_page_get_annot_mapping(page);
-  GList* s;
+
+  GList *annot_list = poppler_page_get_annot_mapping(page);
+  GList *s;
   for (s = annot_list; s != NULL; s = s->next) {
-    PopplerAnnotMapping* m = (PopplerAnnotMapping *)s->data;
+    PopplerAnnotMapping *m = (PopplerAnnotMapping *)s->data;
     int type = poppler_annot_get_annot_type(m->annot);
-    if(type == 1) {
-        char* cont = poppler_annot_get_contents(m->annot);
-        if(cont) {
-            strncat(comm, cont, 1024);
-            strncat(comm, "\n", 1024);
-            g_free(cont);
-        }
-    } 
+    if (type == 1) {
+      char *cont = poppler_annot_get_contents(m->annot);
+      if (cont) {
+        strncat(comm, cont, 1024);
+        strncat(comm, "\n", 1024);
+        g_free(cont);
+      }
+    }
   }
   info->annotations = comm;
-  poppler_page_free_annot_mapping(annot_list);  
-  
+  poppler_page_free_annot_mapping(annot_list);
+
   info->videos = NULL;
-  GList* link_list = poppler_page_get_link_mapping(page);
+  GList *link_list = poppler_page_get_link_mapping(page);
   for (s = link_list; s != NULL; s = s->next) {
-    PopplerLinkMapping* m = (PopplerLinkMapping *)s->data; 
-    PopplerAction* a = m->action;
-    if(a->type == POPPLER_ACTION_LAUNCH) {
-        PopplerActionLaunch* launch = (PopplerActionLaunch*)a;
-//         printf("\n\n%s\n", launch->file_name);    
-        info->videos = strdup(launch->file_name);
+    PopplerLinkMapping *m = (PopplerLinkMapping *)s->data;
+    PopplerAction *a = m->action;
+    if (a->type == POPPLER_ACTION_LAUNCH) {
+      PopplerActionLaunch *launch = (PopplerActionLaunch *)a;
+      //         printf("\n\n%s\n", launch->file_name);
+      info->videos = strdup(launch->file_name);
     }
   }
   poppler_page_free_link_mapping(link_list);
-    
+
   cairo_show_page(img);
 
   cairo_destroy(img);
@@ -75,48 +76,48 @@ int convert(PopplerPage *page, const char *fname, SlideInfo* info) {
   return 0;
 }
 
+// ---------------------------------------------------------------------------
+void progress_cb(int slide) { progress_update(1); }
 
-void progress_cb(int slide) {
-    progress_update(1);
-}
-
-void extract_slide(PopplerDocument* pdffile, int p, SlideInfo* info, Options* options) {
-    PopplerPage *page;
-    char fname[64];
-    sprintf(fname, "slide-%d.svg", p);
-    page = poppler_document_get_page(pdffile, p);
-    convert(page, fname, &(info[p])); 
-    if(options->nonotes) {
-        free(info[p].annotations);
-        info[p].annotations = "";
-    }
-    progress_update(1);
+// ---------------------------------------------------------------------------
+void extract_slide(PopplerDocument *pdffile, int p, SlideInfo *info,
+                   Options *options) {
+  PopplerPage *page;
+  char fname[64];
+  sprintf(fname, "slide-%d.svg", p);
+  page = poppler_document_get_page(pdffile, p);
+  convert(page, fname, &(info[p]));
+  if (options->nonotes) {
+    free(info[p].annotations);
+    info[p].annotations = "";
+  }
+  progress_update(1);
 #if NO_SLIDES
-    char* b64 = strdup(empty_img);
+  char *b64 = strdup(empty_img);
 #else
-    char* b64 = encode_file_base64(fname);
+  char *b64 = encode_file_base64(fname);
 #endif
-    info[p].slide = b64;
-    unlink(fname);
+  info[p].slide = b64;
+  unlink(fname);
 }
 
-
+// ---------------------------------------------------------------------------
 int main(int argc, char *argv[]) {
   Options options = {.single = 0, .presenter = 0, .nonotes = 0, .name = NULL};
   PopplerDocument *pdffile;
   char abspath[PATH_MAX];
   char fname_uri[PATH_MAX + 32];
 
-  if(argc <= 1) {
+  if (argc <= 1) {
     show_usage(argv[0], cli_options);
     return 1;
   }
-  if(parse_cli_options(&options, cli_options, argc, argv)) {
+  if (parse_cli_options(&options, cli_options, argc, argv)) {
     return 1;
   }
-  
-  const char* input = argv[argc - 1];
-  if(access(input, F_OK ) == -1) {
+
+  const char *input = argv[argc - 1];
+  if (access(input, F_OK) == -1) {
     printf_color(1, TAG_FAIL "Could not open file '%s'\n", input);
     return 1;
   }
@@ -132,67 +133,84 @@ int main(int argc, char *argv[]) {
 
   int pages = poppler_document_get_n_pages(pdffile);
   printf_color(1, TAG_OK "Loaded %d slides\n", pages);
-  
-  char fname[1024];
-  snprintf(fname, sizeof(fname), "%s.html", options.name ? options.name : "index");
-  FILE* output = fopen(fname, "w");
-  if(!output) {
-      printf_color(1, TAG_FAIL "Could not create output file [m]index.html[/m]\n");
-      return 1;
-  }
-  
-  printf_color(1, TAG_INFO "Converting slides...\n");
-  progress_start(1, pages * 4, NULL);  
 
-  char* template = read_file("index.html.template");
-  if(!template) {
-    printf_color(1, TAG_FAIL "Could not open file [m]%s[/m]\n", "index.html.template");
+  char fname[1024];
+  snprintf(fname, sizeof(fname), "%s.html",
+           options.name ? options.name : "index");
+  FILE *output = fopen(fname, "w");
+  if (!output) {
+    printf_color(1,
+                 TAG_FAIL "Could not create output file [m]index.html[/m]\n");
     return 1;
   }
-  
-  template = replace_string_first(template, "{{black.svg}}", encode_array_base64(black_svg, black_svg_len));
-  template = replace_string_first(template, "{{freeze.svg}}", encode_array_base64(freeze_svg, freeze_svg_len));
-  template = replace_string_first(template, "{{open.svg}}", encode_array_base64(open_svg, open_svg_len));
+
+  printf_color(1, TAG_INFO "Converting slides...\n");
+  progress_start(1, pages * 4, NULL);
+
+  char *template = read_file("index.html.template");
+  if (!template) {
+    printf_color(1, TAG_FAIL "Could not open file [m]%s[/m]\n",
+                 "index.html.template");
+    return 1;
+  }
+
+  template = replace_string_first(
+      template, "{{black.svg}}",
+      encode_array_base64((char *)black_svg, black_svg_len));
+  template = replace_string_first(
+      template, "{{freeze.svg}}",
+      encode_array_base64((char *)freeze_svg, freeze_svg_len));
+  template =
+      replace_string_first(template, "{{open.svg}}",
+                           encode_array_base64((char *)open_svg, open_svg_len));
 
   SlideInfo info[pages];
-  
+
   // create slide data
   for (int p = 0; p < pages; p++) {
     extract_slide(pdffile, p, info, &options);
   }
-  
-  char* slide_data = encode_array(info, 2, pages, 0, progress_cb);
-  char* video_data = encode_array(info, 1, pages, 1, progress_cb);
-  char* annot_data = encode_array(info, 0, pages, 1, progress_cb);
-  
-  if(options.single) {
-    template = replace_string_first(template, "{{script}}", "<script type='text/javascript'>\n"
-        "var slide_info = {"
-        "'slides': {{slides}},\n"
-        "'videos': {{videos}},\n"
-        "'annotations': {{annotations}}\n"
-        "};\n"
-        "</script>");
-    
+
+  char *slide_data = encode_array(info, 2, pages, 0, progress_cb);
+  char *video_data = encode_array(info, 1, pages, 1, progress_cb);
+  char *annot_data = encode_array(info, 0, pages, 1, progress_cb);
+
+  if (options.single) {
+    template = replace_string_first(template, "{{script}}",
+                                    "<script type='text/javascript'>\n"
+                                    "var slide_info = {"
+                                    "'slides': {{slides}},\n"
+                                    "'videos': {{videos}},\n"
+                                    "'annotations': {{annotations}}\n"
+                                    "};\n"
+                                    "</script>");
+
     template = replace_string_first(template, "{{slides}}", slide_data);
     template = replace_string_first(template, "{{videos}}", video_data);
     template = replace_string_first(template, "{{annotations}}", annot_data);
   } else {
     char include[1024];
-    snprintf(include, sizeof(include), "<script type='text/javascript' src='%s.js'></script>\n", options.name ? options.name : "slides");
+    snprintf(include, sizeof(include),
+             "<script type='text/javascript' src='%s.js'></script>\n",
+             options.name ? options.name : "slides");
     template = replace_string_first(template, "{{script}}", include);
-    snprintf(include, sizeof(include), "%s.js", options.name ? options.name : "slides");
-    FILE* f = fopen(include, "w");
-    fprintf(f, "var slide_info = {'slides': %s,\n'videos': %s,\n'annotations': %s\n};\n", slide_data, video_data, annot_data);
+    snprintf(include, sizeof(include), "%s.js",
+             options.name ? options.name : "slides");
+    FILE *f = fopen(include, "w");
+    fprintf(f,
+            "var slide_info = {'slides': %s,\n'videos': %s,\n'annotations': "
+            "%s\n};\n",
+            slide_data, video_data, annot_data);
     fclose(f);
   }
-  
-  template = replace_string_first(template, "{{presenter}}", options.presenter ? "true" : "false");
-  
+
+  template = replace_string_first(template, "{{presenter}}",
+                                  options.presenter ? "true" : "false");
+
   fwrite(template, strlen(template), 1, output);
   fclose(output);
-  
+
   printf_color(1, TAG_OK "Done!\n");
-  
+
   return 0;
 }
